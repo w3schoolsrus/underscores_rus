878a6dbea72bdacd430b1e34b979482fa80858ad
w3schoolsrus <55655132+w3schoolsrus@users.noreply.github.com>
2020-07-25 09:42:50 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Оба-на! Эта страница не может&rsquo; быть найдена.', '_s' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Оба-на! Эта страница не может быть найдена.', '_s' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
